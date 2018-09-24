f3ed24d4bf35d60be3b1133475280c108a4e97e4
Daniel Illanes <daniel.illanes@truextend.com>
2018-09-24 10:23:08 -0400

    Retrieving ingredients list

--- src/main/java/com/pizzashop/restclient/RestclientApplication.java
+++ src/main/java/com/pizzashop/restclient/RestclientApplication.java
@@ -1,57 +1,83 @@
 package com.pizzashop.restclient;
 
 import com.pizzashop.restclient.models.Ingredient;
-import com.sun.xml.internal.ws.api.pipe.ContentType;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @SpringBootApplication
 public class RestclientApplication {
 
+    public static int timeInterval;
+
     public static void main(String[] args) {
-        final String uri = "http://localhost:8080/api/v1/ingredients/1";
-        /*
+
+        GetIngredient();
+        Random rand = new Random();
+
+        timeInterval = rand.nextInt(10000) + 10000;
+
+        TimerTask repeatedTask = new TimerTask() {
+            public void run() {
+                System.out.println(timeInterval);
+                AddIngredient();
+                timeInterval = rand.nextInt(10000) + 10000;
+            }
+        };
+        Timer timer = new Timer("Timer");
+
+        long delay  = 1000L;
+        long period = 10000;
+        timer.scheduleAtFixedRate(repeatedTask, delay,  timeInterval);
+    }
+
+    public static void AddIngredient(){
+        final String postUri = "http://localhost:8080/api/v1/ingredients";
+
+        Ingredient newIngredient = new Ingredient("Cinnamon");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", String.valueOf(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Ingredient> requestBody = new HttpEntity<>(newIngredient, headers);
+        Ingredient postedIngredient = restTemplate.postForObject(postUri, requestBody, Ingredient.class);
+
+        if (postedIngredient != null && postedIngredient.getName() != null) {
+            System.out.println("ID: " + postedIngredient.getId());
+            System.out.println("Name: " + postedIngredient.getName());
+        } else {
+            System.out.println("Something error!");
+        }
+    }
+
+    public static void GetIngredient(){
+        final String uri = "http://localhost:8080/api/v1/ingredients";
+
+        HttpHeaders headers = new HttpHeaders();
         RestTemplate restTemplate = new RestTemplate();
 
-        // Send request with GET method and default Headers.
-
-        Ingredient[] list = restTemplate.getForObject(uri, Ingredient[].class);
-
-        if (list != null) {
-            for (Ingredient e : list) {
-                System.out.println("Ingredient: " + e.getName() + " - " + e.getId());
-            }
-        }
-        */
-
-        // HttpHeaders
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
-        // Request to return JSON format
+        headers.add("Accept", String.valueOf(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("my_other_key", "my_other_value");
 
-        // HttpEntity<String>: To get result as String.
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-        // RestTemplate
-        RestTemplate restTemplate = new RestTemplate();
+        String results = restTemplate.getForObject(uri,String.class);
 
-        // Send request with GET method, and Headers.
-        ResponseEntity<Ingredient> response = restTemplate.exchange(uri, //
-                HttpMethod.GET, entity, Ingredient.class);
+        ResponseEntity<PagedResources<Ingredient>> pagedResourcesResponseIngredients = restTemplate.exchange(uri, HttpMethod.GET,
+                null, new ParameterizedTypeReference<PagedResources<Ingredient>>() {});
 
-        Ingredient result = response.getBody();
+        PagedResources<Ingredient> pagedResources = pagedResourcesResponseIngredients.getBody();
+        Collection<Ingredient> ingredientsCollection = pagedResources.getContent();
 
-        System.out.println(result.getName());
-
+        for(Ingredient i : ingredientsCollection){
+            System.out.println(i.getId());
+            System.out.println(i.getName());
+            System.out.println("");
+        }
     }
 }
